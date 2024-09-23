0de766b678a1fbd15f7345f11422d5d7831c9413
Alex Muiruri <content@alexmuiruri.com>
2024-09-23 12:17:26 +0300

    Debugged errors in all the pages that expected arguments but got none

--- app/(dashboard)/billings/page.tsx
+++ app/(dashboard)/billings/page.tsx
@@ -6,17 +6,21 @@ import AddProduct from '@/components/forms/addproduct'
 import ProductsCard from '@/components/cards/productscard'
 import { getManyProducts } from '@/actions/productController'
 import { getBusiness } from '@/actions/businessController'
+import { getManyCategories } from '@/actions/categoryController'
+import { getManyBrands } from '@/actions/brandController'
 
 const BillingsPage = async () => {
     const session = await auth()
     const business = await getBusiness(session?.user?.id as string)
     if (!business) return redirect('/settings')
-    const products = await getManyProducts() ?? []
+    const products = await getManyProducts(business.id) ?? []
+    const categories = await getManyCategories(business.id) ?? []
+    const brands     = await getManyBrands(business.id) ?? []
     return (
         <div className="page-wrapper">
             <PageHeader title='Inventory' description={String(products.length)} >
                 <input type="text" className="bg-transparent focus-within:!ring-0 border text-sm ps-5 py-2" placeholder="Search" />
-                <AddProduct />
+                <AddProduct business={business} brands={brands} categories={categories}  />
             </PageHeader>
             <div className="page-body">
                 <ProductsCard products={products} />
