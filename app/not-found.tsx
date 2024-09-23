c7e819867f093291a5a4c5b7f43187be72c43fdd
Alex Muiruri <content@alexmuiruri.com>
2024-09-23 12:36:28 +0300

    Debugged errors in all the pages that expected arguments but got none

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,14 +5,16 @@ import Sidebar from '@/components/layouts/Sidebar'
 import Navbar from '@/components/layouts/Navbar'
 import Footer from '@/components/layouts/Footer'
 import NotFoundPage from '@/components/layouts/notfoundpage'
+import { getBusiness } from '@/actions/businessController'
 
 
 const NotFound = async () => {
     const session = await auth()
     if(!session?.user) redirect('/login')
+    const business = await getBusiness(session?.user?.id as string) ?? undefined
     return (
         <main className="main relative md:w-[calc(100%_-_15rem)] md:left-52 mx-3 md:mx-0 md:ms-4 top-px">
-            <Sidebar />
+            <Sidebar business={business} />
             <div className='main-wrapper'>
                 <Navbar />
                 <div className="container-fluid relative mt-2 mb-6 min-h-[calc(100vh_-_8.5rem_-_2px)]">
