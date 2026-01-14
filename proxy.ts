edf0343b06235701444a2e0eb53982e68abbb253
Alex Muiruri <content@alexmuiruri.com>
2026-01-14 16:35:54 +0300

    thi

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,12 @@
-export { auth as middleware } from "auth"
+import { NextRequest, NextResponse } from 'next/server'
+
+export { auth as middleware } from 'auth'
 
 // Or like this if you need to do something here.
-// export default auth((req) => {
-//   console.log(req.auth) //  { session: { user: { ... } } }
-// })
-
+export function proxy(request: NextRequest) {
+    return NextResponse.redirect(new URL('/home', request.url))
+}
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
